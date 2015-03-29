6f84f1db253325897925d68310dc7bfa1b9662c0
Travis Kriplean <tkriplean@gmail.com>
2015-03-29 09:25:23 -0700

    when computing document position, account for css transform translations

--- scripts/coords.js
+++ scripts/coords.js
@@ -8,8 +8,35 @@ Castanaut.Coords = Castanaut.Coords || {
       do {
         curleft += obj.offsetLeft;
         curtop += obj.offsetTop;
+
+        var style = getComputedStyle(obj),
+                    transform = style.transform || style.webkitTransform || style.mozTransform;
+
+        // handle css transform translations
+        if(transform) {
+
+          // modified from http://stackoverflow.com/questions/21912684
+          var mat = transform.match(/^matrix3d\((.+)\)$/), x = 0, y = 0;
+          if(mat){ 
+            x = parseFloat(mat[1].split(', ')[12]);
+            y = parseFloat(mat[1].split(', ')[13]);
+          }
+          else{
+            mat = transform.match(/^matrix\((.+)\)$/);
+            if(mat) {
+              x = parseFloat(mat[1].split(', ')[4]);
+              y = parseFloat(mat[1].split(', ')[5]);
+            } 
+
+          }
+
+          curleft += x
+          curtop += y
+        }
+
       } while (obj = obj.offsetParent);
     }
+
     return [curleft,curtop,origObj.offsetWidth,origObj.offsetHeight];
   },
 
@@ -17,6 +44,7 @@ Castanaut.Coords = Castanaut.Coords || {
     var pos = Castanaut.Coords.documentPos(obj);
     pos[0] -= window.scrollX;
     pos[1] -= window.scrollY;
+
     if (pos[0] > window.innerWidth || pos[0] < 0) {
       pos[0] = -1;
     }
